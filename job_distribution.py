8cee8011613aa995b51f412d6735f4f36aafb51b
niranjaniprasad <niranjaniprasad@Niranjanis-MacBook-Pro.local>
2017-01-11 23:29:31 -0500

    switching bi-modal, periodic distributions

--- job_distribution.py
+++ job_distribution.py
@@ -1,5 +1,5 @@
-import numpy as np
-
+import numpy as np 
+import math
 
 class Dist:
 
@@ -9,6 +9,7 @@ class Dist:
         self.job_len = job_len
 
         self.job_small_chance = 0.8
+        self.job_period = 5
 
         self.job_len_big_lower = job_len * 2 / 3
         self.job_len_big_upper = job_len
@@ -22,7 +23,11 @@ class Dist:
         self.other_res_lower = 1
         self.other_res_upper = max_nw_size / 5
 
-        self.switch_chance = 0.05
+        self.normal = 0
+        self.bimodal = 1
+        self.periodic = 0
+
+        #self.switch_chance = 0.8
 
     def normal_dist(self):
 
@@ -60,7 +65,6 @@ class Dist:
 
         return nw_len, nw_size
 
-
 def generate_sequence_work(pa, seed=42):
 
     np.random.seed(seed)
@@ -69,22 +73,68 @@ def generate_sequence_work(pa, seed=42):
 
     nw_dist = pa.dist.bi_model_dist
 
-    nw_len_seq = np.zeros(simu_len, dtype=int)
-    nw_size_seq = np.zeros((simu_len, pa.num_res), dtype=int)
+    nw_len_seq = np.zeros((pa.simu_len, pa.num_ex), dtype=int)
+    nw_size_seq = np.zeros((pa.simu_len, pa.num_ex, pa.num_res), dtype=int)
 
-    for i in range(simu_len):
+    if pa.dist.normal:
 
-        if np.random.rand() < pa.new_job_rate:  # a new job comes
-
-            if pa.nonStationary: 
-                if np.random.rand() < pa.dist.switch_chance: # switch duration distribution
-                    pa.dist.job_small_chance = 1 - pa.dist.job_small_chance
+        for i in range(simu_len):
 
             nw_len_seq[i], nw_size_seq[i, :] = nw_dist()
 
-    nw_len_seq = np.reshape(nw_len_seq,
-                            [pa.num_ex, pa.simu_len])
-    nw_size_seq = np.reshape(nw_size_seq,
-                             [pa.num_ex, pa.simu_len, pa.num_res])
+    else:
+
+        for i in range(pa.simu_len):
+            #set parameters of length dist for cycle i:
+
+            if pa.dist.bimodal:
+
+                if np.random.rand() < 0.5:
+                    pa.dist.job_small_chance = 1 - pa.dist.job_small_chance
+
+            elif pa.dist.periodic:
+                    pa.dist.job_period = np.random.randint(2, 10)
+
+            for j in range(pa.num_ex):
+                #generate length, size attributes of sequence j in cycle i:
+
+                if pa.dist.bimodal:
+
+                    nw_len_seq[i, j], nw_size_seq[i, j, :] = pa.dist.bi_model_dist()
+    
+                elif pa.dist.periodic:
+
+                    #nw_len_seq[i,j] = round(4*(math.sin(0.5*j) + math.cos(0.25*j))+8)
+                    offset = np.random.randint(-2, 2)
+                    nw_len_seq[i,j] = round(7*(math.sin((j+offset)/pa.dist.job_period))+8)
+
+                    for k in range(pa.num_res):
+                        nw_size_seq[i,j,k] = np.random.randint(1, pa.dist.max_nw_size + 1)
+
+    # for i in range(simu_len):
+
+    #     if np.random.rand() < pa.new_job_rate:  # a new job comes
+
+    #         periodic_dist = 0;
+
+    #         if periodic_dist:
+
+    #             nw_len_seq[i] = round(4*(math.sin(0.5*i) + math.cos(0.25*i))+8)
+
+    #             for j in range(pa.num_res):
+    #                 nw_size_seq[i,j] = np.random.randint(1, pa.dist.max_nw_size + 1)
+
+    #         else:
+
+    #             nw_len_seq[i], nw_size_seq[i, :] = nw_dist()
+
+    # nw_len_seq = np.reshape(nw_len_seq,
+    #                         [pa.num_ex, pa.simu_len])
+    # nw_size_seq = np.reshape(nw_size_seq,
+    #                          [pa.num_ex, pa.simu_len, pa.num_res])
+
+    #print 'hi', nw_len_seq
+    #print 'hey', nw_size_seq[0,:]
 
     return nw_len_seq, nw_size_seq
+
