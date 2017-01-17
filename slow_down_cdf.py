a05b0be7547b23f1a6ea5395d815d07f60a9fa14
seffari <ari.seff@gmail.com>
2017-01-16 19:26:12 -0500

    saving slowdowns and work complete

--- slow_down_cdf.py
+++ slow_down_cdf.py
@@ -186,6 +186,7 @@ def launch(pa, pg_resume=None, render=False, plot=False, repre='image', end='no_
     # ---- Parameters ----
 
     test_types = ['Tetris', 'SJF', 'Random']
+    traj_types = ["Regular", "Hallucinated"]
 
     if pg_resume is not None:
         test_types = ['PG'] + test_types
@@ -200,95 +201,114 @@ def launch(pa, pg_resume=None, render=False, plot=False, repre='image', end='no_
     num_job_remain = {}
     job_remain_delay = {}
 
+    pa.unseen = True
+    if pa.unseen:
+        test_types = ['PG']
+
     for test_type in test_types:
-        all_discount_rews[test_type] = []
-        jobs_slow_down[test_type] = []
-        work_complete[test_type] = []
-        work_remain[test_type] = []
-        job_len_remain[test_type] = []
-        num_job_remain[test_type] = []
-        job_remain_delay[test_type] = []
+        for traj_type in traj_types:
+            all_discount_rews[test_type, traj_type] = []
+            jobs_slow_down[test_type, traj_type] = []
+            work_complete[test_type, traj_type] = []
+            work_remain[test_type, traj_type] = []
+            job_len_remain[test_type, traj_type] = []
+            num_job_remain[test_type, traj_type] = []
+            job_remain_delay[test_type, traj_type] = []
 
     for seq_idx in xrange(pa.num_ex):
-        print('\n\n')
-        print("=============== " + str(seq_idx) + " ===============")
+        for traj_type in traj_types :
+            print('\n\n')
+            print("=============== " + str(seq_idx) + " ===============")
 
-        test_types = ['PG']
-        for test_type in test_types:
+            for test_type in test_types:
 
-            print "Regular version"
-            rews, info = get_traj(test_type, pa, env, pa.episode_max_length, pg_resume)
+                if traj_type == "Regular":
+                    print "Regular version"
+                    rews, info = get_traj(test_type, pa, env, pa.episode_max_length, pg_resume)
 
-            print "---------- " + test_type + " -----------"
+                    print "---------- " + test_type + " -----------"
 
-            print "total discount reward : \t %s" % (discount(rews, pa.discount)[0])
+                    print "total discount reward : \t %s" % (discount(rews, pa.discount)[0])
 
-            print " "
-            print "Hallucinated version"
-            rews, info, env = get_traj_halluc(test_type, pa, env, pa.episode_max_length, pg_resume)
-
-            print "---------- " + test_type + " -----------"
-
-            print "total discount reward : \t %s" % (discount(rews, pa.discount)[0])
+                print " "
 
 
+                if pa.unseen and traj_type == "Hallucinated":
+                    env.seq_no -= 1
+                    print "Hallucinated version"
+                    rews, info, env = get_traj_halluc(test_type, pa, env, pa.episode_max_length, pg_resume)
+
+                    print "---------- " + test_type + " -----------"
+
+                    print "total discount reward : \t %s" % (discount(rews, pa.discount)[0])
 
 
-            all_discount_rews[test_type].append(
-                discount(rews, pa.discount)[0]
-            )
 
-            # ------------------------
-            # ---- per job stat ----
-            # ------------------------
 
-            enter_time = np.array([info.record[i].enter_time for i in xrange(len(info.record))])
-            finish_time = np.array([info.record[i].finish_time for i in xrange(len(info.record))])
-            job_len = np.array([info.record[i].len for i in xrange(len(info.record))])
-            job_total_size = np.array([np.sum(info.record[i].res_vec) for i in xrange(len(info.record))])
+                all_discount_rews[test_type, traj_type].append(
+                    discount(rews, pa.discount)[0]
+                )
 
-            finished_idx = (finish_time >= 0)
-            unfinished_idx = (finish_time < 0)
+                # ------------------------
+                # ---- per job stat ----
+                # ------------------------
 
-            jobs_slow_down[test_type].append(
-                (finish_time[finished_idx] - enter_time[finished_idx]) / job_len[finished_idx]
-            )
-            work_complete[test_type].append(
-                np.sum(job_len[finished_idx] * job_total_size[finished_idx])
-            )
-            work_remain[test_type].append(
-                np.sum(job_len[unfinished_idx] * job_total_size[unfinished_idx])
-            )
-            job_len_remain[test_type].append(
-                np.sum(job_len[unfinished_idx])
-            )
-            num_job_remain[test_type].append(
-                len(job_len[unfinished_idx])
-            )
-            job_remain_delay[test_type].append(
-                np.sum(pa.episode_max_length - enter_time[unfinished_idx])
-            )
+                enter_time = np.array([info.record[i].enter_time for i in xrange(len(info.record))])
+                finish_time = np.array([info.record[i].finish_time for i in xrange(len(info.record))])
+                job_len = np.array([info.record[i].len for i in xrange(len(info.record))])
+                job_total_size = np.array([np.sum(info.record[i].res_vec) for i in xrange(len(info.record))])
 
-        env.seq_no = (env.seq_no + 1) % env.pa.num_ex
+                finished_idx = (finish_time >= 0)
+                unfinished_idx = (finish_time < 0)
 
-    # -- matplotlib colormap no overlap --
-    if plot:
-        num_colors = len(test_types)
-        cm = plt.get_cmap('gist_rainbow')
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.set_color_cycle([cm(1. * i / num_colors) for i in range(num_colors)])
+                jobs_slow_down[test_type, traj_type].append(
+                    (finish_time[finished_idx] - enter_time[finished_idx]) / job_len[finished_idx]
+                )
 
-        for test_type in test_types:
-            slow_down_cdf = np.sort(np.concatenate(jobs_slow_down[test_type]))
-            slow_down_yvals = np.arange(len(slow_down_cdf))/float(len(slow_down_cdf))
-            ax.plot(slow_down_cdf, slow_down_yvals, linewidth=2, label=test_type)
+                print "job_len ", job_len
 
-        plt.legend(loc=4)
-        plt.xlabel("job slowdown", fontsize=20)
-        plt.ylabel("CDF", fontsize=20)
-        # plt.show()
-        plt.savefig(pg_resume + "_slowdown_fig" + ".pdf")
+                work_complete[test_type, traj_type].append(
+                    np.sum(job_len[finished_idx] * job_total_size[finished_idx])
+                )
+                work_remain[test_type, traj_type].append(
+                    np.sum(job_len[unfinished_idx] * job_total_size[unfinished_idx])
+                )
+                job_len_remain[test_type, traj_type].append(
+                    np.sum(job_len[unfinished_idx])
+                )
+                num_job_remain[test_type, traj_type].append(
+                    len(job_len[unfinished_idx])
+                )
+                job_remain_delay[test_type, traj_type].append(
+                    np.sum(pa.episode_max_length - enter_time[unfinished_idx])
+                )
+
+            env.seq_no = (env.seq_no + 1) % env.pa.num_ex
+
+            print "mean slowdown " , np.mean(jobs_slow_down['PG', traj_type][seq_idx])
+            print "work complete" ,  work_complete['PG', traj_type][seq_idx]
+
+    # # -- matplotlib colormap no overlap --
+    # if plot:
+    #     num_colors = len(test_types)
+    #     cm = plt.get_cmap('gist_rainbow')
+    #     fig = plt.figure()
+    #     ax = fig.add_subplot(111)
+    #     ax.set_color_cycle([cm(1. * i / num_colors) for i in range(num_colors)])
+
+    #     for test_type in test_types:
+    #         slow_down_cdf = np.sort(np.concatenate(jobs_slow_down[test_type]))
+    #         slow_down_yvals = np.arange(len(slow_down_cdf))/float(len(slow_down_cdf))
+    #         ax.plot(slow_down_cdf, slow_down_yvals, linewidth=2, label=test_type)
+
+    #     plt.legend(loc=4)
+    #     plt.xlabel("job slowdown", fontsize=20)
+    #     plt.ylabel("CDF", fontsize=20)
+    #     # plt.show()
+    #     plt.savefig(pg_resume + "_slowdown_fig" + ".pdf")
+
+    np.save('jobs_slow_down.npy', jobs_slow_down)
+    np.save('work_complete.npy', work_complete) 
 
     return all_discount_rews, jobs_slow_down
 
